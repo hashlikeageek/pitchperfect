33e5b9fc944b7e2303587d88f1b244e4970e2b1f
Ashish Kumar Sai <ashishkumarsai@me.com>
2016-12-08 16:30:24 +0530

    Everything is working fine till now ( Added the Outlets plus two actions) and images.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButtonClicked: UIButton!
+    @IBOutlet weak var recordingInProgressText: UILabel!
+    @IBOutlet weak var stopRecordingClicked: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        stopRecordingClicked.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button Clicked")
+        recordingInProgressText.text = "Recording In Progress"
+        stopRecordingClicked.isEnabled = true
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording Hit")
+        recordButtonClicked.isEnabled = false
+    }
+    
 
 }
 
