03a82fe5f6194253d860b1c4bcb2788d2e1d0503
Ashish Kumar Sai <ashishkumarsai@me.com>
2016-12-09 19:09:52 +0530

    Finally working, need to tweak a few things though.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -31,6 +31,7 @@ class PlaySoundsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAudio()
 
         // Do any additional setup after loading the view.
     }
